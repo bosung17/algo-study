52df25eeb122fee2cd47a6b05ff4deae42c329b3
ytv9802 <ytv9802@gmail.com>
2024-08-28 21:04:52 +0900

    과제 5개

--- 240828/src/퀵정렬_로무토파티션.java
+++ 240828/src/퀵정렬_로무토파티션.java
@@ -1,4 +1,42 @@
+import java.util.Arrays;
 
 public class 퀵정렬_로무토파티션 {
-
+	static int[] arr = {12, 15, 32, 45, 123, 5, 12, 55};
+	static int N = arr.length;
+	
+	public static void main(String[] args) {
+		quickSort(0, N-1);
+		
+		System.out.println(Arrays.toString(arr));
+	}
+	
+	static void quickSort(int left, int right) {
+		if (left < right) {
+			int pivot = partition(left, right);
+			quickSort(left, pivot-1);
+			quickSort(pivot+1, right);
+		}
+	}
+	
+	static int partition(int left, int right) {
+		int pivot = arr[right];
+		int i = left - 1;
+		for (int j = left; j < right; j++) {
+			if (arr[j] < pivot) {
+				i++;
+				int temp = arr[i];
+				arr[i] = arr[j];
+				arr[j] = temp;
+			}
+		}
+		arr[right] = arr[i+1];
+		arr[i+1] = pivot;
+		return i + 1;
+	}
 }
+
+//3 6 4 3 7 2 5
+//3 4 6 3 7 2 5
+//3 4 3 6 7 2 5
+//3 4 3 2 7 6 5
+//      i   j
--- 240828/src/퀵정렬_호어파티션.java
+++ 240828/src/퀵정렬_호어파티션.java
@@ -1,9 +1,13 @@
+import java.util.Arrays;
+
 public class 퀵정렬_호어파티션 {
-	static int[] arr = {69, 10, 30, 8, 2, 16};
+	static int[] arr = {15, 23, 53, 54, 12, 123, 53, 5};
 	static int N = arr.length;
 	
 	public static void main(String[] args) {
+		quickSort(0, N-1);
 		
+		System.out.println(Arrays.toString(arr));
 	}
 	
 	static void quickSort(int left, int right) {
@@ -23,8 +27,23 @@ public class 퀵정렬_호어파티션 {
 		while (L <= R) {
 			// R이 마지막 인덱스일 때 L이 인덱스 에러가 나지 않도록 L <= R 조건 한 번 더 설정
 			while (L <= R && arr[L] <= pivot) {
-				// 여기 쓰는 중
+				L++;
+			}
+			while (arr[R] > pivot) {
+				R--;
+			}
+			
+			if (L < R) {
+				int temp = arr[L];
+				arr[L] = arr[R];
+				arr[R] = temp;
 			}
 		}
+		
+		int temp = arr[left];
+		arr[left] = arr[R];
+		arr[R] = temp;
+		
+		return R;
 	}
 }
