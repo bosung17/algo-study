ef5ecc15ff940e0e8065ea5dc1f8f026327f2b38
ytv9802 <ytv9802@gmail.com>
2024-09-03 17:46:46 +0900

    0903 백준 푸는 중

--- 240903/src/BOJ_11727_2xn_타일링2/Main.java
+++ 240903/src/BOJ_11727_2xn_타일링2/Main.java
@@ -1,10 +1,9 @@
-package BOJ_11726_2xn_타일링;
+package BOJ_11727_2xn_타일링2;
 
 import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-		
 		Scanner sc = new Scanner(System.in);
 		
 		int n = sc.nextInt();
@@ -12,10 +11,10 @@ public class Main {
 		int[] dp = new int[1001];
 		
 		dp[1] = 1;
-		dp[2] = 2;
+		dp[2] = 3;
 		
 		for (int i = 3; i <= 1000; i++) {
-			dp[i] = (dp[i-1] + dp[i-2])%10007;
+			dp[i] = (dp[i-1] + dp[i-2] * 2)%10007;
 		}
 		
 		System.out.println(dp[n]);
